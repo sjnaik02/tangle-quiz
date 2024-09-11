e729ce8b51a5556c60878ed4753132988a468557
Shourya Naik <sjnaik02@gmail.com>
2024-09-11 12:31:32 +0530

    minor styling improvements

--- app/BetaCallout.js
+++ app/BetaCallout.js
@@ -3,14 +3,22 @@ import React from "react";
 const BetaCallout = () => {
   return (
     <div
-      className="bg-red-100 border-l-4 border-l-red-400 text-red-800 px-4 py-3 rounded relative max-w-3xl mx-auto mt-8"
+      className="relative mx-auto mt-8 w-full max-w-3xl rounded border-l-4 border-l-red-400 bg-red-100 px-4 py-3 text-base text-red-800"
       role="alert"
     >
       <strong className="font-bold">Notice:</strong>
       <span className="block sm:inline">
         {" "}
         This quiz is currently in development. Please forward all feedback and
-        issues to shourya@readtangle.com.
+        issues to{" "}
+        <a
+          href="mailto:shourya@readtangle.com"
+          target="_blank"
+          className="underline"
+        >
+          me
+        </a>
+        .
       </span>
     </div>
   );
--- app/ProgressBar.js
+++ app/ProgressBar.js
@@ -3,9 +3,13 @@ import React from "react";
 import { Progress } from "@/components/ui/progress";
 
 const ProgressBar = ({ answered, total }) => (
-  <div className="sticky top-0 bg-white p-4 z-10 border border-gray-300">
-    <Progress value={(answered / total) * 100} className="h-2" />
-    <p className="text-sm text-gray-600 mt-2 text-right">{`${answered} of ${total} questions answered`}</p>
+  <div className="sticky top-0 z-10 bg-white px-3 py-3 shadow-md sm:px-4">
+    <div className="flex items-center justify-between">
+      <div className="mr-4 w-full">
+        <Progress value={(answered / total) * 100} className="h-1.5" />
+      </div>
+      <p className="whitespace-nowrap text-xs text-gray-600 sm:text-sm">{`You've answered ${answered} of ${total}`}</p>
+    </div>
   </div>
 );
 
